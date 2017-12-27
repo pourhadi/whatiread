3ee24355ffc9575180d0ab15109a26e32b769814
Daniel Pourhadi <dan@pourhadi.com>
2017-12-27 05:08:19 -0600

    fix qry

--- src/main/kotlin/com/pourhadi/wir/data/dao/BtcDao.kt
+++ src/main/kotlin/com/pourhadi/wir/data/dao/BtcDao.kt
@@ -4,7 +4,7 @@ import org.jdbi.v3.sqlobject.statement.SqlQuery
 
 interface BtcDao {
 
-    @SqlQuery("select value from btc where code = :code AND redeemed != 1")
+    @SqlQuery("select value from btc where code = :code AND redeemed = 0")
     fun getValueForCode(code: String): Int
 
 }
